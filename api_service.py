0993faba27a08f33adc0a952f11cc8d9998275f7
Anshum Singla <singlaanshum0786@gmail.com>
2025-11-27 20:11:06 +0530

    changes

--- api_service.py
+++ api_service.py
@@ -58,7 +58,7 @@ GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth'
 GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token'
 GOOGLE_USERINFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo'
 # Use environment variable for redirect URI (supports Vercel deployment)
-BACKEND_URL = os.environ.get('BACKEND_URL', 'http://localhost:5000')
+BACKEND_URL = os.environ.get('BACKEND_URL')
 REDIRECT_URI = f"{BACKEND_URL.rstrip('/')}/auth/google/callback"
 SCOPES = [
     'openid',
